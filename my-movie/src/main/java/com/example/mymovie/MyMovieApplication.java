9941f9a899d71f450bcaf7c2f851fa5deaef0aac
votre Nom <alexandre.motbal2@gmail.com>
2022-11-16 18:07:20 +0100

    Actuator works

--- my-movie/src/main/java/com/example/mymovie/MyMovieApplication.java
+++ my-movie/src/main/java/com/example/mymovie/MyMovieApplication.java
@@ -2,14 +2,11 @@ package com.example.mymovie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
 public class MyMovieApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyMovieApplication.class, args);
 	}
-
 }
